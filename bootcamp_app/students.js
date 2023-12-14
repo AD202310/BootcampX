639f09c44cd51da25e43c75fa58df6870016c22f
AD202310 <a.g.dychenko@gmail.com>
2023-12-13 23:17:11 -0500

    Parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-let parameter1 = process.argv[2];
-let parameter2 = process.argv[3];
+const parameter1 = process.argv[2];
+const parameter2 = process.argv[3];
+const values = [`%${parameter1}%`, parameter2]
+
 pool.query(`
 SELECT students.id, students.name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${parameter1}%'
-LIMIT ${parameter2};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`,
+values
+)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,8 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-let parameter1 = process.argv[2];
+const parameter1 = process.argv[2];
+const values = [`${parameter1}`]
 
 pool.query(`
 SELECT  cohorts.name as cohort, teachers.name as teacher
@@ -15,10 +16,10 @@ FROM teachers
 JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${parameter1}'
+WHERE cohorts.name = $1
 GROUP BY  cohorts.name, teachers.name
 ORDER BY teacher ASC;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
